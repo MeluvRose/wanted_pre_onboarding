6a6152346c05e5fc2c4e37b758c0c27a54543917
meluvrose <tanggu3910@gmail.com>
2022-04-17 16:10:11 +0900

    edit urls

--- preonboarding/urls.py
+++ preonboarding/urls.py
@@ -16,14 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-# http://127.0.0.1/
-# http://127.0.0.1/app/
-
-# http://127.0.0.1/create/
-# http://127.0.0.1/read/1/
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('myapp.urls'))
     path('', include('funding.urls'))
 ]
